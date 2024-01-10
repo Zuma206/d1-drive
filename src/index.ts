6665e19024a49b37e5f4f637b178441e21d2f02c
Zuma <zuma@zuma.eu.org>
2024-01-10 23:47:21 +0000

    Improved error handing & added GET page

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,18 @@
 import { z } from "zod";
 
 const parser = z.object({
-	secret: z.string(),
-	database: z.string(),
-	query: z.string(),
-	params: z.array(z.unknown()).optional(),
-	method: z
-		.union([
-			z.literal("all"),
-			z.literal("first"),
-			z.literal("raw"),
-			z.literal("run"),
-		])
-		.optional(),
+  secret: z.string(),
+  database: z.string(),
+  query: z.string(),
+  params: z.array(z.unknown()).optional(),
+  method: z
+    .union([
+      z.literal("all"),
+      z.literal("first"),
+      z.literal("raw"),
+      z.literal("run"),
+    ])
+    .optional(),
 });
 
 type Env = Record<string, string | D1Database | undefined>;
@@ -21,31 +21,45 @@ const err = (err: string, status = 500) => Response.json({ err }, { status });
 const res = (res: unknown, status = 200) => Response.json({ res }, { status });
 
 export default {
-	async fetch(request: Request, env: Env) {
-		const command = parser.safeParse(await request.json());
-		if (!command.success) return err(command.error.issues[0].message, 400);
+  async fetch(request: Request, env: Env) {
+    if (request.method !== "POST" && request.method !== "GET") {
+      return err("Method must be POST", 405);
+    } else if (request.method == "GET") {
+      return new Response(`"Hello, World!" - D1 Drive`);
+    }
 
-		const database = env[command.data.database];
-		const secret = env[command.data.database + ":SECRET"];
+    const body = await request.json().catch(() => null);
+    const command = parser.safeParse(body);
+    if (!command.success) {
+      const { message, path } = command.error.issues[0];
+      const code = path.reduce(
+        (accumulator, current) => accumulator + `[${JSON.stringify(current)}]`,
+        "body"
+      );
+      return err(`${code}: ${message}`);
+    }
 
-		if (secret !== command.data.secret) return err("Invalid secret", 401);
-		if (typeof database !== "object" || !("prepare" in database)) {
-			return err("Database not found", 404);
-		}
+    const database = env[command.data.database];
+    const secret = env[command.data.database + ":SECRET"];
 
-		const statement = database
-			.prepare(command.data.query)
-			.bind(command.data.params);
+    if (secret !== command.data.secret) return err("Invalid secret", 401);
+    if (typeof database !== "object" || !("prepare" in database)) {
+      return err("Database not found", 404);
+    }
 
-		switch (command.data.method) {
-			case "all":
-				return res(await statement.all());
-			case "first":
-				return res(await statement.first());
-			case "raw":
-				return res(await statement.raw());
-			default:
-				return res(await statement.run());
-		}
-	},
+    const statement = database
+      .prepare(command.data.query)
+      .bind(command.data.params);
+
+    switch (command.data.method) {
+      case "all":
+        return res(await statement.all());
+      case "first":
+        return res(await statement.first());
+      case "raw":
+        return res(await statement.raw());
+      default:
+        return res(await statement.run());
+    }
+  },
 };
